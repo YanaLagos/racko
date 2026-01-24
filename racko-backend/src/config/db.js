ee8d19b5657a80cd33f77e2d643e04d73e0bdfff
YanaLagos <yaney.lagos@alumno.iplacex.cl>
2026-01-24 00:04:16 -0300

    Fix DATABASE

--- racko-backend/src/config/db.js
+++ racko-backend/src/config/db.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
   port: Number(process.env.MYSQLPORT || 3306), 
   user: process.env.MYSQLUSER, 
   password: process.env.MYSQLPASSWORD, 
-  database: process.env.MYSQLDATABASE,
+  database: process.env.MYSQL_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
